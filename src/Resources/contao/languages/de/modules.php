2cbac68a148ca4271483344c2108deb4c2d7880d
Martin Schwenzer <mail@derhaeuptling.com>
2018-10-08 16:43:36 +0200

    added some german translations

---
+++ src/Resources/contao/languages/de/modules.php
@@ -0,0 +1,20 @@
+<?php
+
+/*
+ * Regiondo Bundle for Contao Open Source CMS.
+ *
+ * @copyright  Copyright (c) 2018, derhaeuptling
+ * @author     Codefog <https://codefog.pl>
+ * @license    MIT
+ */
+
+/**
+ * Backend modules.
+ */
+$GLOBALS['TL_LANG']['MOD']['regiondo_products'] = ['Regiondo products'];
+
+/*
+ * Frontend modules
+ */
+$GLOBALS['TL_LANG']['FMD']['regiondo'] = 'Regiondo';
+$GLOBALS['TL_LANG']['FMD']['regiondo_cart_widget'] = ['Regiondo cart (widget)'];
---
+++ src/Resources/contao/languages/de/tl_regiondo_product.php
@@ -0,0 +1,34 @@
+<?php
+
+/*
+ * Regiondo Bundle for Contao Open Source CMS.
+ *
+ * @copyright  Copyright (c) 2018, derhaeuptling
+ * @author     Codefog <https://codefog.pl>
+ * @license    MIT
+ */
+
+$GLOBALS['TL_LANG']['tl_regiondo_product']['product'] = ['Product', 'Please choose the Regiondo product.'];
+$GLOBALS['TL_LANG']['tl_regiondo_product']['lastSync'] = ['Last synchronize', 'The last date when product was synchronized.'];
+$GLOBALS['TL_LANG']['tl_regiondo_product']['name'] = ['Name', 'Here you can enter your optional product name. If none set, the Regiondo product name will be used.'];
+$GLOBALS['TL_LANG']['tl_regiondo_product']['obsolete'] = ['Obsolete record', 'The record is no longer present in Regiondo.'];
+
+/*
+ * Legends
+ */
+$GLOBALS['TL_LANG']['tl_regiondo_product']['product_legend'] = 'Product and name';
+
+/*
+ * Buttons
+ */
+$GLOBALS['TL_LANG']['tl_regiondo_product']['new'] = ['New product', 'Create a new product'];
+$GLOBALS['TL_LANG']['tl_regiondo_product']['show'] = ['Product details', 'Show the details of product ID %s'];
+$GLOBALS['TL_LANG']['tl_regiondo_product']['edit'] = ['Edit product', 'Edit product ID %s'];
+$GLOBALS['TL_LANG']['tl_regiondo_product']['delete'] = ['Delete product', 'Delete product ID %s'];
+$GLOBALS['TL_LANG']['tl_regiondo_product']['synchronize'] = ['Synchronize products', 'Synchronize the products'];
+
+/*
+ * Reference
+ */
+$GLOBALS['TL_LANG']['tl_regiondo_product']['syncConfirm'] = 'Successfully synchronized Regiondo products (%s created, %s updated, %s marked as obsolete).';
+$GLOBALS['TL_LANG']['tl_regiondo_product']['syncError'] = 'Regiondo products could not be synchronized.';
